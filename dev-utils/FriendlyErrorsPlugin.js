e534f9e218aa069f22baea3dac3e24e2d91d7f3a
Daniel Nehring <nehrdani@gmail.com>
2018-03-08 20:12:11 +0100

    create compiler webpack plugin

---
+++ dev-utils/FriendlyErrorsPlugin.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const clearConsole = require('react-dev-utils/clearConsole')
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages')
+const logger = require('./logger')
+
+let WEBPACK_COMPILING = false
+let WEBPACK_DONE = false
+
+// This is a custom Webpack Plugin that prints out prettier console messages
+// and errors depending on webpack compiler events. It runs on the Node.js
+// server webpack instance.
+class WebpackErrorsPlugin {
+  constructor (options = {}) {
+    this.verbose = options.verbose
+    this.onSuccessMessage = options.onSuccessMessage
+    this.target = options.target === 'web' ? 'CLIENT' : 'SERVER'
+  }
+
+  apply (compiler) {
+    compiler.plugin('done', stats => {
+      const rawMessages = stats.toJson({}, true)
+      const messages = formatWebpackMessages(rawMessages)
+      WEBPACK_COMPILING = false
+      if (!messages.errors.length && !messages.warnings.length) {
+        if (!WEBPACK_DONE) {
+          if (!this.verbose) {
+            clearConsole()
+          }
+          logger.done('Compiled successfully')
+          WEBPACK_DONE = true
+
+          if (this.onSuccessMessage) {
+            logger.log(this.onSuccessMessage)
+            logger.log('')
+          }
+        }
+      }
+
+      if (
+        messages.errors.length &&
+        !(
+          rawMessages.errors &&
+          rawMessages.errors.length > 0 &&
+          (rawMessages.errors[0].includes('assets.json') ||
+            rawMessages.errors[0].includes("Module not found: Can't resolve"))
+        )
+      ) {
+        messages.errors.forEach(e => {
+          logger.error(
+            `Failed to compile ${this.target} with ${messages.errors
+              .length} errors`,
+            e
+          )
+        })
+      }
+
+      if (messages.warnings.length) {
+        logger.warn(
+          `Compiled with ${messages.warnings.length} warnings`
+        )
+        messages.warnings.forEach(w => logger.log(w))
+      }
+    })
+
+    compiler.plugin('invalid', () => {
+      WEBPACK_DONE = false
+      if (!WEBPACK_COMPILING) {
+        if (!this.verbose) {
+          clearConsole()
+        }
+        logger.start('Compiling...')
+        WEBPACK_COMPILING = true
+      }
+    })
+  }
+}
+
+module.exports = WebpackErrorsPlugin
