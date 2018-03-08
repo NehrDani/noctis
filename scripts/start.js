5122e508d7aa73d201a11d4ff8adea4aab000e38
Daniel Nehring <nehrdani@gmail.com>
2018-03-08 21:17:33 +0100

    compile client and start dev server

--- scripts/start.js
+++ scripts/start.js
@@ -3,18 +3,6 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.NODE_ENV = 'development'
 
-const webpack = require('webpack')
-const createConfig = require('../config/createConfig')
-
-const createCompiler = (webpack, config) => {
-  try {
-    return webpack(config)
-  } catch (err) {
-    console.log('Failed to compile.')
-    process.exit(1)
-  }
-}
-
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
@@ -22,16 +10,52 @@ process.on('unhandledRejection', err => {
   throw err
 })
 
-console.log('Compiling...')
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const clearConsole = require('react-dev-utils/clearConsole')
+const createConfig = require('../config/createConfig')
+const createDevServerConfig = require('../config/createDevServerConfig')
+const logger = require('../dev-utils/logger')
+const createCompiler = require('../dev-utils/createCompiler')
 
-// Create dev configs using config factory.
-const serverConfig = createConfig('node', process.env.NODE_ENV, {})
+const isInteractive = process.stdout.isTTY
 
-// Create compilers
+// Tools like Cloud9 rely on this.
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000
+const HOST = process.env.HOST || '0.0.0.0'
+
+// Optimistically, we make the console look exactly like the output of our
+// FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
+if (isInteractive) {
+  clearConsole()
+}
+
+logger.start('Compiling...')
+
+// Create dev configs using config our factory.
+const serverConfig = createConfig('node', 'dev')
+const clientConfig = createConfig('web', 'dev')
+
 const serverCompiler = createCompiler(webpack, serverConfig)
 
-// Start server webpack instance in watch mode.
-// Error handling is done inside the compiler instance
+// Start our server webpack instance in watch mode.
 serverCompiler.watch({
   quiet: true
 }, () => {})
+
+// Compile our assets with webpack
+const clientCompiler = createCompiler(webpack, clientConfig)
+
+// Create a new instance of Webpack-dev-server for our client assets.
+// This will actually run on a different port than the users app.
+const clientDevServer = new WebpackDevServer(clientCompiler, createDevServerConfig())
+
+// Start Webpack-dev-server
+clientDevServer.listen(
+  DEFAULT_PORT + 1,
+  err => {
+    if (err) {
+      logger.error(err)
+    }
+  }
+)
