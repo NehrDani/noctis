0cdee76db7289a5500baa02f5842ded2133493c9
Daniel Nehring <nehrdani@gmail.com>
2018-03-09 10:00:19 +0100

    add more description

--- config/createConfig.js
+++ config/createConfig.js
@@ -146,6 +146,11 @@ module.exports = (
 
     config.entry = {
       client: [
+        // Include an alternative client for WebpackDevServer. A client's job is to
+        // connect to WebpackDevServer by a socket and get notified about changes.
+        // When you save a file, the client will either apply hot updates (in case
+        // of CSS changes), or refresh the page (in case of JS changes). When you
+        // make a syntax error, this client will display a syntax error overlay.
         require.resolve('../dev-utils/webpackHotDevClient'),
         path.resolve(paths.appSrc, 'client.js')
       ]
