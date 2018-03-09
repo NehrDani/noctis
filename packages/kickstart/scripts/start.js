5485e61808eda4b34add2f80f3bc176524b07fb5
Daniel Nehring <nehrdani@gmail.com>
2018-03-09 21:36:07 +0100

    refactor start script to be async

--- packages/kickstart/scripts/start.js
+++ packages/kickstart/scripts/start.js
@@ -17,8 +17,13 @@ const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const clearConsole = require('react-dev-utils/clearConsole')
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles')
+const {
+  choosePort,
+  prepareUrls,
+} = require('react-dev-utils/WebpackDevServerUtils')
 const logger = require('kickstart-dev-utils/logger')
 const createCompiler = require('kickstart-dev-utils/createCompiler')
+const printInstructions = require('kickstart-dev-utils/printInstructions')
 const createConfig = require('../config/createConfig')
 const createDevServerConfig = require('../config/createDevServerConfig')
 const paths = require('../config/paths')
@@ -30,43 +35,69 @@ if (!checkRequiredFiles([paths.appServerJs, paths.appClientJs])) {
   process.exit(1)
 }
 
-// Tools like Cloud9 rely on this.
-const port = parseInt(process.env.PORT, 10) || 3000
-const host = process.env.HOST || '0.0.0.0'
-const protocol = process.env.HTTPS ? 'https' : 'http'
+const start = async () => {
+  const host = process.env.HOST || '0.0.0.0'
+  const defaultPort = parseInt(process.env.PORT, 10) || 3000
+  const defaultPortDev = defaultPort + 1
+  const protocol = process.env.HTTPS ? 'https' : 'http'
 
-// Optimistically, we make the console look exactly like the output of our
-// FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
-if (isInteractive) {
-  clearConsole()
+  // We attempt to use the default port but if it is busy, we offer the user to
+  // run on a different port. `choosePort()` Promise resolves to the next free port.
+  const port = await choosePort(host, defaultPort)
+  const portDev = await choosePort(host, defaultPortDev)
+
+  if (!port || !portDev) {
+    // We have not found a port.
+    return
+  }
+
+  // We do this before importing the wepack.config.client.dev otherwise
+  // PORT and PORT_DEV won't be set at new webpack.DefinePlugin(env.stringified)
+  process.env.PORT = port
+  process.env.PORT_DEV = portDev
+
+  // Set publicPath for development
+  const publicPath = `${protocol}://${host}:${portDev}`
+
+  // Set callback for compile success
+  const urls = prepareUrls(protocol, host, port)
+  const onCompileSuccess = () => printInstructions(urls)
+
+  // Create dev configs using config our factory.
+  const serverConfig = createConfig('node', 'dev', publicPath, onCompileSuccess)
+  const clientConfig = createConfig('web', 'dev', publicPath, onCompileSuccess)
+
+  // Optimistically, we make the console look exactly like the output of our
+  // FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
+  if (isInteractive) {
+    clearConsole()
+  }
+
+  logger.start('Compiling...')
+
+  const serverCompiler = createCompiler(webpack, serverConfig)
+
+  // Start our server webpack instance in watch mode.
+  serverCompiler.watch({
+    quiet: true
+  }, () => {})
+
+  // Compile our assets with webpack
+  const clientCompiler = createCompiler(webpack, clientConfig)
+
+  // Create a new instance of Webpack-dev-server for our client assets.
+  // This will actually run on a different port than the users app.
+  const clientDevServer = new WebpackDevServer(
+    clientCompiler,
+    createDevServerConfig({ protocol, host, port: portDev })
+  )
+
+  // Start Webpack-dev-server
+  clientDevServer.listen(portDev, err => {
+    if (err) {
+      logger.error(err)
+    }
+  })
 }
 
-logger.start('Compiling...')
-
-// Create dev configs using config our factory.
-const serverConfig = createConfig('node', 'dev')
-const clientConfig = createConfig('web', 'dev')
-
-const serverCompiler = createCompiler(webpack, serverConfig)
-
-// Start our server webpack instance in watch mode.
-serverCompiler.watch({
-  quiet: true
-}, () => {})
-
-// Compile our assets with webpack
-const clientCompiler = createCompiler(webpack, clientConfig)
-
-// Create a new instance of Webpack-dev-server for our client assets.
-// This will actually run on a different port than the users app.
-const clientDevServer = new WebpackDevServer(
-  clientCompiler,
-  createDevServerConfig({ host, port, protocol })
-)
-
-// Start Webpack-dev-server
-clientDevServer.listen(port + 1, err => {
-  if (err) {
-    logger.error(err)
-  }
-})
+start()
