72ef5c74ef36ab43e0d9139888c4bc3ec5bea844
Daniel Nehring <nehrdani@gmail.com>
2018-03-10 17:34:51 +0100

    enforce comma-dangling

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,10 @@ module.exports = {
   ],
   rules: {
     'comma-dangle': ['error', {
-      arrays: 'only-multiline',
-      objects: 'only-multiline',
-      imports: 'only-multiline',
-      exports: 'only-multiline',
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
       functions: 'ignore'
     }],
     'no-console': 'off'
