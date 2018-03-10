6ac1b950d7ca228820f37d1fc2da90b593463408
Daniel Nehring <nehrdani@gmail.com>
2018-03-10 22:59:04 +0100

    improve webpack config factory for prod

--- packages/kickstart/config/createConfig.js
+++ packages/kickstart/config/createConfig.js
@@ -2,10 +2,12 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const StartServerPlugin = require('start-server-webpack-plugin')
 const autoprefixer = require('autoprefixer')
+const nodeExternals = require('webpack-node-externals')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const StartServerPlugin = require('start-server-webpack-plugin')
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
 const FriendlyErrorsPlugin = require('kickstart-dev-utils/FriendlyErrorsPlugin')
 const paths = require('./paths')
 
@@ -22,7 +24,10 @@ module.exports = (
   const IS_DEV = env === 'dev'
 
   const babelOptions = {
-    babelrc: true,
+    babelrc: false,
+    // This is a feature of `babel-loader` for webpack (not Babel itself).
+    // It enables caching results in ./node_modules/.cache/babel-loader/
+    // directory for faster rebuilds.
     cacheDirectory: true,
     presets: [require.resolve('babel-preset-kickstart')],
   }
@@ -32,7 +37,7 @@ module.exports = (
     importLoaders: 1,
     localIdentName: IS_PROD
       ? '[hash:base64:7]'
-      : '[path]__[name]__[local]--[hash:base64:5]',
+      : '[path]__[name]__[local]',
   }
   const postCssOptions = {
     // Necessary for external CSS imports to work
@@ -70,6 +75,38 @@ module.exports = (
       rules: [
         // Disable require.ensure as it's not a standard language feature.
         { parser: { requireEnsure: false } },
+        {
+          // "oneOf" will traverse all following loaders until one will
+          // match the requirements. When no loader matches it will fall
+          // back to the "file" loader at the end of the loader list.
+          oneOf: [
+            // "url" loader works like "file" loader except that it embeds assets
+            // smaller than specified limit in bytes as data URLs to avoid requests.
+            // A missing `test` is equivalent to a match.
+            {
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              loader: require.resolve('url-loader'),
+              options: {
+                limit: 10000,
+                name: '[name].[hash:8].[ext]',
+              },
+            },
+            // (S)CSS Modules support
+            {
+              test: /\s?css$/,
+              exclude: [paths.appBuild],
+              use: IS_PROD
+                ? ExtractTextPlugin.extract({
+                  fallback: {
+                    loader: require.resolve('style-loader'),
+                    options: { hmr: false },
+                  },
+                  use: cssLoaders,
+                })
+                : cssLoaders,
+            },
+          ],
+        },
         // Transform ES6 with Babel
         {
           test: /\.js$/,
@@ -84,25 +121,24 @@ module.exports = (
             },
           ],
         },
-        // CSS Modules support
+        // "file" loader makes sure assets end up in the `build` folder.
+        // When you `import` an asset, you get its filename.
+        // This loader doesn't use a "test" so it will catch all modules
+        // that fall through the other loaders.
         {
-          test: /\s?css$/,
-          exclude: [paths.appBuild],
-          use: IS_PROD
-            ? ExtractTextPlugin.extract({
-              fallback: {
-                loader: require.resolve('style-loader'),
-              },
-              use: cssLoaders,
-            })
-            : cssLoaders,
+          loader: require.resolve('file-loader'),
+          // Exclude `js` files to keep "css" loader working as it injects
+          // it's runtime that would otherwise be processed through "file" loader.
+          // Also exclude `html` and `json` extensions so they get processed
+          // by webpacks internal loaders.
+          exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+          options: {
+            name: '[name].[hash:8].[ext]',
+          },
         },
       ],
     },
     plugins: [
-      // This makes debugging much easier as webpack will add filenames to
-      // modules.
-      new webpack.NamedModulesPlugin(),
       // Prevent creating multiple chunks for the server.
       IS_NODE && new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1,
@@ -111,8 +147,23 @@ module.exports = (
       IS_NODE && IS_DEV && new StartServerPlugin({
         name: 'server.js',
       }),
+      // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+      IS_WEB && !IS_DEV && new ExtractTextPlugin({
+        filename: '[name].[contenthash:8].css',
+      }),
+      // Add module names to factory functions so they appear in browser profiler.
+      IS_DEV && new webpack.NamedModulesPlugin(),
       // Add hot module replacement
       IS_DEV && new webpack.HotModuleReplacementPlugin(),
+      // Watcher doesn't work well if you mistype casing in a path so we use
+      // a plugin that prints an error when you attempt to do this.
+      // See https://github.com/facebook/create-react-app/issues/240
+      IS_DEV && new CaseSensitivePathsPlugin(),
+      // If you require a missing module and then `npm install` it, you still have
+      // to restart the development server for Webpack to discover it. This plugin
+      // makes the discovery automatic so you don't have to restart.
+      // See https://github.com/facebookincubator/create-react-app/issues/186
+      IS_DEV && new WatchMissingNodeModulesPlugin(paths.appNodeModules),
       // Supress errors to console (we use our own logger)
       IS_DEV && new webpack.NoEmitOnErrorsPlugin(),
       // Use our own FriendlyErrorsPlugin during development.
@@ -122,6 +173,15 @@ module.exports = (
         onSuccess: onCompileSuccess,
       }),
     ].filter(Boolean),
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+      dgram: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      child_process: 'empty',
+    },
   }
 
   if (IS_NODE) {
@@ -145,7 +205,7 @@ module.exports = (
 
     config.entry = {
       client: [
-        // Ship a few polyfills by default.
+        // Ship a few polyfills by default:
         require.resolve('./polyfills'),
         // Include an alternative client for WebpackDevServer. A client's job is to
         // connect to WebpackDevServer by a socket and get notified about changes.
@@ -153,19 +213,21 @@ module.exports = (
         // of CSS changes), or refresh the page (in case of JS changes). When you
         // make a syntax error, this client will display a syntax error overlay.
         require.resolve('kickstart-dev-utils/webpackHotDevClient'),
+        // Finally, this is your app's code:
         path.resolve(paths.appClientJs),
+        // We include the app code last so that if there is a runtime error during
+        // initialization, it doesn't blow up the WebpackDevServer client, and
+        // changing JS code would still trigger a refresh.
       ],
     }
 
     // Configure client-side bundles output. Note the public path is set to 3000
     config.output = {
-      path: path.resolve(paths.appClientBuild),
-      publicPath,
+      // Add /* filename */ comments to generated require()s in the output.
       pathinfo: true,
       filename: '[name].js',
       chunkFilename: '[name].chunk.js',
-      devtoolModuleFilenameTemplate: ({ resourcePath }) =>
-        path.resolve(resourcePath).replace(/\\/g, '/'),
+      publicPath,
     }
   }
 
@@ -182,6 +244,15 @@ module.exports = (
       publicPath,
       filename: `${filename}.js`,
       chunkFilename: `${filename}.chunk.js`,
+    }
+  }
+
+  if (IS_DEV) {
+    // Turn off performance hints during development because we don't do any
+    // splitting or minification in interest of speed. These warnings become
+    // cumbersome.
+    config.performance = {
+      hints: false,
     }
   }
 
