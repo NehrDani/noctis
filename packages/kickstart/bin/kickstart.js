89949023481d710e56de6c5812778581845de827
Daniel Nehring <nehrdani@gmail.com>
2018-03-11 01:47:36 +0100

    fix indentation problems

--- packages/kickstart/bin/kickstart.js
+++ packages/kickstart/bin/kickstart.js
@@ -21,18 +21,20 @@ if (isScript(script)) {
   )
 
   if (signal === 'SIGKILL') {
-    console.log(`
-      The build failed because the process exited too early.
-      This probably means the system ran out of memory or someone called 'kill -9' on the process.
-    `)
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'This probably means the system ran out of memory or someone called ' +
+        '`kill -9` on the process.'
+    )
     process.exit(1)
   }
 
   if (signal === 'SIGTERM') {
-    console.log(`
-      The build failed because the process exited too early.
-      Someone might have called 'kill' or 'killall', or the system could be shutting down.
-    `)
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'Someone might have called `kill` or `killall`, or the system could ' +
+        'be shutting down.'
+    )
     process.exit(1)
   }
 
