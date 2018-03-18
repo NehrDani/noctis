78440bf69c4380b4140436a252e9ee93c47a8771
Daniel Nehring <nehrdani@gmail.com>
2018-03-18 22:07:34 +0100

    add scripts during preparation

--- packages/create-kickstart-app/utils/prepare.js
+++ packages/create-kickstart-app/utils/prepare.js
@@ -6,6 +6,10 @@ module.exports = (base, appName) => {
     name: appName,
     version: '0.1.0',
     private: true,
+    scripts: {
+      start: 'kickstart start',
+      build: 'kickstart build',
+    },
   }
 
   ensureDirSync(base)
