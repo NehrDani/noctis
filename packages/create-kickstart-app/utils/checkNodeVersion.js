a53fcfee723d8e0da478c0c493b2dfedc1e65d58
Daniel Nehring <nehrdani@gmail.com>
2018-03-18 17:48:00 +0100

    check node version for usage of Kickstart

---
+++ packages/create-kickstart-app/utils/checkNodeVersion.js
@@ -0,0 +1,18 @@
+
+const semver = require('semver')
+const { unsupportedNodeVersion } = require('../utils/messages')
+
+module.exports = engines => {
+  const nodeVersion = engines && engines.node
+
+  if (!nodeVersion) {
+    return
+  }
+
+  if (!semver.satisfies(process.version, nodeVersion)) {
+    console.error(
+      unsupportedNodeVersion(process.version, semver.coerce(nodeVersion))
+    )
+    process.exit(1)
+  }
+}
