d510efc9f19c5e1d3ac51623a42ad37dd90fc2de
Daniel Nehring <nehrdani@gmail.com>
2018-03-22 09:37:53 +0100

    move logic to starter

--- packages/create-kickstart-app/utils/createPackageJson.js
+++ packages/create-kickstart-app/utils/createPackageJson.js
@@ -1,7 +1,7 @@
-const { ensureDirSync, writeFileSync } = require('fs-extra')
+const { writeFile } = require('fs-extra')
 const path = require('path')
 
-module.exports = (base, appName) => {
+module.exports = async (appPath, appName) => {
   const packageJson = {
     name: appName,
     version: '0.1.0',
@@ -12,12 +12,8 @@ module.exports = (base, appName) => {
     },
   }
 
-  ensureDirSync(base)
-
-  writeFileSync(
-    path.join(base, 'package.json'),
+  await writeFile(
+    path.join(appPath, 'package.json'),
     `${JSON.stringify(packageJson, null, 2)}\n`
   )
-
-  process.chdir(base)
 }
