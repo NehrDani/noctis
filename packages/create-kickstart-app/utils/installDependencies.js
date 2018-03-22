75b3f7cf9c07c0b5de5cbe1d7a034f3d0a85d8c1
Daniel Nehring <nehrdani@gmail.com>
2018-03-22 09:39:04 +0100

    use more propiate name

--- packages/create-kickstart-app/utils/installDependencies.js
+++ packages/create-kickstart-app/utils/installDependencies.js
@@ -1,21 +1,26 @@
 const chalk = require('chalk')
 const { spawn } = require('child_process')
+const ora = require('ora')
+const shouldUseYarn = require('./shouldUseYarn')
 
 const dependencies = ['react', 'react-dom', '@nehrdani/kickstart', 'express']
+const spinner = ora()
 
-const getInstallArgs = (packages, useYarn) =>
-  useYarn
-    ? ['add', ...packages]
-    : ['install', '--save', '--loglevel', 'error', ...packages].filter(Boolean)
-
-module.exports = (base, verbose, useYarn) =>
+module.exports = (base, useNpm) =>
   new Promise((resolve, reject) => {
+    const useYarn = shouldUseYarn(useNpm)
     const cmd = useYarn ? 'yarnpkg' : 'npm'
-    const args = getInstallArgs(dependencies, useYarn)
+    const args = useYarn
+      ? ['add', ...dependencies]
+      : ['install', '--save', '--loglevel', 'error', ...dependencies]
 
-    const child = spawn(cmd, args, { stdio: verbose ? 'inherit' : 'ignore' })
+    const child = spawn(cmd, args, { stdio: 'ignore' })
+
+    spinner.start()
 
     child.on('close', code => {
+      spinner.stop()
+
       if (code !== 0) {
         const command = `${cmd} ${args.join(' ')}`
         return reject(new Error(`  ${chalk.cyan(command)} has failed.`))
